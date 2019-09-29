6d2756515be97e507e029724bc60c8fd9b296bde
Vincenq <457145556@qq.com>
2019-09-29 22:16:24 +0800

    图片上传

--- e3-parent/e3-manager-web/src/main/java/cn/itheima/manager/web/controller/PictrueController.java
+++ e3-parent/e3-manager-web/src/main/java/cn/itheima/manager/web/controller/PictrueController.java
@@ -1,36 +1,48 @@
 package cn.itheima.manager.web.controller;
 
 import cn.itheima.commons.controller.BaseController;
-import cn.itheima.manager.interfaces.IItemCatService;
-import cn.itheima.manager.po.EasyUITreeNode;
+import cn.itheima.commons.utils.FastDFSClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-@RequestMapping(value="itemCatController")
+@RequestMapping(value="pictrueController")
 @Controller
 public class PictrueController extends BaseController {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
+    @Value("IMAGE_SERVER_URL")
+    private String IMAGE_SERVER_URL;
 
-    @RequestMapping(value="selectItemInfoById")
+    @RequestMapping(value="uploadFile")
     @ResponseBody
-    public List<EasyUITreeNode> getItemCatList() {
+    public Map uploadFile(MultipartFile uploadFile) {
         Map<String, Object> map = new HashMap<>();
         try {
-
+            //把图片上传的图片服务器
+            FastDFSClient fastDFSClient = new FastDFSClient("classpath:conf/client.conf");
+            //取文件扩展名
+            String originalFilename = uploadFile.getOriginalFilename();
+            String extName = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
+            //得到一个图片的地址和文件名
+            String url = fastDFSClient.uploadFile(uploadFile.getBytes(), extName);
+            //补为完整的url
+            url = IMAGE_SERVER_URL+url;
+            //封装到map中返回
+            map.put("error",0);
+            map.put("url",url);
         } catch (Exception e) {
             logger.error("图片上传失败异常", e);
             e.printStackTrace();
             map.put(_MESSAGE, "图片上传失败");
+            map.put("error",1);
         }
         return map;
     }
